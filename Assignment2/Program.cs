b731d23935002bc8394bf3503809d7f71b96e82f
aby85 <abyantony85@gmail.com>
2024-02-17 15:49:01 -0500

    assignment completed

---
+++ Assignment2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Interval
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a real number:");
+        double x;
+
+        if (double.TryParse(Console.ReadLine(), out x))
+        {
+            bool isInInterval =
+                ((x >= 2 && x < 3)
+                || (x > 0 && x <= 1)
+                || (x > -10 && x <= -2));
+
+            if (isInInterval)
+            {
+                Console.WriteLine("x belongs to I");
+            }
+            else
+            {
+                Console.WriteLine("x does not belong to I");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid real number.");
+        }
+    }
+}
